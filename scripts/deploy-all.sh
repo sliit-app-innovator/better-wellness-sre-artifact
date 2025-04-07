d7bc9c5393a71c7357040ea006b0c6ae5d8bb831
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-07 13:26:20 +0530

    ext ingress apply

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -3,11 +3,8 @@
 BASE_PATH="/home/ec2-user/better-wellness-sre-artifact"
 SCRIPTS_PATH="$BASE_PATH/scripts"
 
-#echo "🔧 Initiating deployment namespace..."
-kubectl apply -f $BASE_PATH/global/namespace.yaml
-
-echo "🔐 Creating image pull secret (if needed)..."
-kubectl apply -f $BASE_PATH/ecr/regcred-secret.yml
+#echo "🔐 Creating image pull secret (if needed)..."
+#kubectl apply -f $BASE_PATH/ecr/regcred-secret.yml
 
 echo "🐘 Deploying Redis..."
 kubectl apply -f $BASE_PATH/redis-service/
