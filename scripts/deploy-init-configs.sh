0e06d40f12e70aa9604d1e538e155693e1a6fb0e
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-07 13:08:15 +0530

    ext ingress apply

--- scripts/deploy-init-configs.sh
+++ scripts/deploy-init-configs.sh
@@ -3,9 +3,6 @@
 BASE_PATH="/home/ec2-user/better-wellness-sre-artifact"
 SCRIPTS_PATH="$BASE_PATH/scripts"
 
-echo "🔧 Initiating deployment namespace..."
-kubectl apply -f $BASE_PATH/global/namespace.yaml
-
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 061039760516.dkr.ecr.ap-south-1.amazonaws.com
 
 aws ecr get-login-password --region ap-south-1 | \
