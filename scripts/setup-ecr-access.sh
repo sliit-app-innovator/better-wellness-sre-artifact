575b4abcbdeeb328b947fc98fcc4e3cca1480b57
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-07 13:34:18 +0530

    ext ingress apply

--- scripts/setup-ecr-access.sh
+++ scripts/setup-ecr-access.sh
@@ -11,4 +11,5 @@ aws ecr get-login-password --region ap-south-1 | \
 kubectl create secret docker-registry ecr-regcred \
 --docker-server=061039760516.dkr.ecr.ap-south-1.amazonaws.com \
 --docker-username=AWS \
---docker-password=$(aws ecr get-login-password --region ap-south-1)
+--docker-password=$(aws ecr get-login-password --region ap-south-1)\
+--namespace=better-wellness
