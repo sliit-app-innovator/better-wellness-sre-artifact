206adb1edc0722cded91946284e405fbc5fcf4ec
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-15 10:21:32 +0530

    api dev

---
+++ scripts/init-components.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BASE_PATH="/home/ec2-user/better-wellness-sre-artifact"
+SCRIPTS_PATH="$BASE_PATH/scripts
+
+echo "🐘 Deploying Redis..."
+kubectl apply -f $BASE_PATH/redis-service/
+
+echo "🧠 Deploying User Service..."
+bash $SCRIPTS_PATH/deploy-user-service.sh
+
+echo "🧠 Deploying Database Service..."
+bash $SCRIPTS_PATH/mysql-deployment.yaml
