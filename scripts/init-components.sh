b4eade53745116eea77d2bd82a114660e08d2050
dsulochana <damith.sulochana@oloidtechnologies.com>
2025-04-15 10:24:43 +0530

    api dev

--- scripts/init-components.sh
+++ scripts/init-components.sh
@@ -7,3 +7,6 @@ kubectl apply -f $BASE_PATH/redis-service/
 
 echo "🧠 Deploying Database Service..."
 bash $SCRIPTS_PATH/mysql-deployment.yaml
+
+echo "🐘 Deploying kubeneties Namespas..."
+kubectl apply -f $BASE_PATH/global/namespace.yaml
